7ca51d8f75fe55d422f63f04d9ae7c7ef5d3f2c7
pallavinikam201 <pallavi.nikam21052019@gmail.com>
2025-05-15 11:37:37 +0530

    new code updated for test

--- src/Login/loginData.java
+++ src/Login/loginData.java
@@ -5,6 +5,8 @@ public class loginData {
 	public static void main(String[] args) {
 		
 		System.out.println("you have login successfully");
+		System.out.println("you have login successfully");
+
 	}
 
 }
