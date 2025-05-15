db800ec26b6980f28aa090726f5d808052cb3155
pallavinikam201 <pallavi.nikam21052019@gmail.com>
2025-05-15 11:05:18 +0530

    added new code for addition

--- src/Registeration/Test.java
+++ src/Registeration/Test.java
@@ -4,9 +4,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		
+		int a,b,c;
+		a=100;
+		b=200;
+		c=a+b;
+		
 		System.out.println("Hello");
 		System.out.println("Jobito");
-		System.out.println("for testing");
 		
 	}
 
