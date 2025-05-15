98d1d15f2cf31db9551fd9d46cb694a7b006101f
pallavinikam201 <pallavi.nikam21052019@gmail.com>
2025-05-15 10:55:10 +0530

    New Code Updated

---
+++ src/Registeration/Test.java
@@ -0,0 +1,13 @@
+package Registeration;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		System.out.println("Jobito");
+		System.out.println("testclass");
+		System.out.println("Testing");
+	}
+
+}
